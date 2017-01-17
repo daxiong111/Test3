71bc5df3e5f71ca8008417f3c0b229331642a89c
daxiong111 <369408609@qq.com>
2017-01-17 16:33:54 +0800

    新增

---
+++ house-electricity/src/components/Main/ChartContent/MainNine.vue
@@ -0,0 +1,184 @@
+<template>
+    <div id="main9"></div>
+</template>
+<script>
+import echarts from 'echarts'
+export default {
+    name: 'MainNine',
+    data: function () {
+        return {
+            chart: null,
+            data:[]
+        }
+    },
+    created: function() {
+        const url = "static/data/dataMap.json";
+        this.$http.get(url).then(function(data){
+            const JsonArr=data.body.resultObj;
+            this.$set(this, 'data', JsonArr);
+            this.drawMain9('main9');
+        },function(response){
+            console.info(response);
+        })
+    },
+    methods: {
+        drawMain9 (id){
+            this.chart = echarts.init(document.getElementById(id));
+            this.chart.setOption({
+                tooltip : {
+                    trigger: 'item',
+                    //show: true,   //default true
+                    showDelay: 0,
+                    hideDelay: 50,
+                    transitionDuration: 0,
+                    backgroundColor: 'rgba(255,255,255,0.6)',   //提示窗背景色
+                    borderColor: '#bbddff',
+                    borderRadius: 2,
+                    borderWidth: 1,   //提示窗边框
+                    padding: 8,
+                    position: function (p) {
+                        // 位置回调
+                        // console.log && console.log(p);
+                        return [p[0] + 10, p[1] - 10];
+                    },
+                    textStyle: {   //提示窗文字
+                        color: '#3d4f7f',
+                        decoration: 'none',
+                        fontFamily: '微软雅黑, sans-serif',
+                        fontSize: 12,
+                        //fontStyle: 'italic',
+                        //fontWeight: 'bold'
+                    },
+                }, //提示窗完
+//色块
+                legend: {
+                    show: false,
+                    orient: 'vertical',
+                    left: 'left',
+                    data:['用电量分布']
+                },
+//色块
+                visualMap: {     //色块样式
+                    y: '2',
+                    x2: '2',
+                    backgroundColor: 'rgba(19,35,75,0.7)', //提示窗背景色
+                    borderColor: '#9eb4e4',
+                    padding:15,
+                    itemGap:13,      //色块间距
+                    itemWidth:20,    //色块宽度
+                    itemHeight:10,   //色块高度
+                    borderWidth: 0.5,
+                    splitNumber: 5,
+                    pieces: [
+                        //{min: 1500}, // 不指定 max，表示 max 为无限大（Infinity）。
+                        {min: 1000, max: 2000, label: '1000 到 2000（自定义label）'},
+                        {min: 600, max: 1000, label: '600 到 1000（自定义label）'},
+                        {min: 300, max: 600, label: '300 到 600（自定义label）'},
+                        {min: 0, max: 300, label: '0到 300（自定义label）'},
+                        //{value: 123, label: '123（自定义特殊颜色）', color: 'grey'}, // 表示 value 等于 123 的情况。
+                        //{max: 10}      // 不指定 min，表示 min 为无限大（-Infinity）。
+                    ],
+                    inRange: {
+                        color: ['#7fc4d3', '#7faad3', '#7f97d3', '#7f7fd3']
+                    },
+                    textStyle: {
+                        fontSize: '12',
+                        fontFamily: '微软雅黑',
+                        //fontWeight : 'bold'
+                        color: '#cbddfd'
+                    }
+                },
+                series: [{
+                    name: '用电量分布',
+                    type: 'map',
+                    mapType: '湖北',
+                    z: 2,
+                    //center: [115.97, 29.71],     //当前视角的中心点，用经纬度表示
+                    left: 'center',    //左侧外边距
+                    top: 'center',
+                    right: 'auto',
+                    bottom: 'auto',
+                    layoutCenter: null,    //['30%', '30%']
+                    layoutSize: '',       //地图的大小
+                    mapValueCalculation: 'sum',     //多个拥有相同地图类型的系列会使用同一个地图展现，如果多个系列都在同一个区域有值  'sum' 取和,'average' 取平均值,'max' 取最大值,'min' 取最小值。
+                    // showLegendSymbol: '用电量分布',     //相应区域显示图例的颜色标识（系列标识的小圆点），存在 legend 组件时生效
+                    scaleLimit: {     //滚轮缩放的极限控制，通过min, max最小和最大的缩放值，默认的缩放为1。
+                        min: 0,
+                        max: 1,
+                    },
+                    selectedMode: false,     //选中模式，表示是否支持多个选中，默认关闭
+                    roam: true,    //鼠标缩放和平移。默认不开启。只开启缩放或平移，可以设置 'scale' 或者 'move'。设置 true 都开启
+                    label: {       //地图上的文本标签
+                        normal: {
+                            show: true,
+                            textStyle: {
+                                color: '#fff',
+                                fontStyle: 'normal',
+                                fontWeight: 'normal',
+                                fontFamily: '微软雅黑',
+                                fontSize: 12
+                            }
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                color: '#c2ddfd',
+                                fontStyle: 'normal',
+                                fontWeight: 'normal',
+                                fontFamily: '微软雅黑',
+                                fontSize: 12,
+                            }
+                        }
+                    },
+                    itemStyle: {       //地图样式
+                        normal: {
+                            areaColor: '#3e5080',    //区域的颜色
+                            borderColor: '#3d4f7f', //模块边框颜色
+                            borderWidth: 1,
+                            color: '#ffddaf', //点的颜色
+                            borderType: 'solid',
+                            shadowBlur: '',
+                            shadowColor: '',
+                            shadowOffsetX: 0,
+                            shadowOffsetY: 0,
+                            opacity: 0.9
+                        },
+                        emphasis: {
+                            areaColor: '#3d4f7f',
+                            color: 'rgba(255,255,255,0.3)',
+                            borderColor: "#cbddfd", //模块边框变色
+                            borderWidth: 2,
+                            borderType: 'solid',
+                            shadowBlur: '',
+                            shadowColor: '',
+                            shadowOffsetX: 0,
+                            shadowOffsetY: 0,
+                            opacity: 1
+                        }
+                    },
+                    data: this.data
+                }]
+            })
+        }
+    },
+    mounted() {
+        this.$nextTick(function() {
+            this.drawMain9('main9');
+            var that = this;
+            var resizeTimer = null;
+            window.onresize = function() {
+                if (resizeTimer) clearTimeout(resizeTimer);
+                resizeTimer = setTimeout(function() {
+                    that.drawMain9('main9');
+                }, 100);
+            }
+        });
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #main9{width:100%; height:578px;}
+</style>
---
+++ house-electricity/src/components/Main/ContentFour.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="col_4">   <!--湖北省用电情况-->
+        <div class="widget widget_map">
+            <h2 class="widget_title">  <p>湖北省用电情况</p>  </h2>
+            <div class="widget_content">
+                <div class="padd padd_ydqk">
+                    <ul class="switch_btn switch_btn_map">  <!--切换按钮-->
+                        <li v-for="(tab, index) in tabs" :class="{'switch_btn_click':index===checked}" @click="tabChange(index)" >{{tab.name}}</li>
+                    </ul>
+                    <div class="chars_ydqk">    <!--用电情况_地图-->
+                        <main-nine></main-nine>
+                    </div>
+                    <div class="table_ydqk">   <!--用电情况_右边表格-->
+                        <table width="100%" border="0" cellspacing="1" cellpadding="0">
+                            <thead>
+                            <tr class="table_title">
+                                <td width="24%">城市</td>
+                                <td width="45%">用电量&nbsp;(kWh)</td>
+                                <td width="31%">电费&nbsp;(元)</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="(item, index) in items">
+                                <td>{{item.name}}</td>
+                                <td>{{item.mount}}</td>
+                                <td>{{item.fee}}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import '../../assets/js/hubei';
+    import MainNine from './ChartContent/MainNine'
+    export default {
+        name: 'ContentFour',
+        components: {
+            MainNine
+        },
+        data: function () {
+            return {
+                checked:0,
+                tabs:[
+                    {name: "全部", id: '0'},
+                    {name: "本年", id: '1'},
+                    {name: "本月", id: '1'},
+                    {name: "本日", id: '1'}
+                ],
+                items:[]
+            }
+        },
+        created: function() {
+            const url = "static/data/mapTable.json";
+            this.$http.get(url).then(function(data){
+                const JsonArr=data.body.resultObj;
+                this.$set(this, 'items',JsonArr);
+//                console.log(this.items)
+            },function(response){
+                console.info(response);
+            })
+        },
+        methods:{
+            tabChange:function(index){
+                this.checked = index;
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .col_4{ width:66.666%; height:620px;  margin-top: -6px;float:left;}
+    /*模块********************************************************************************/
+    .widget{ margin:6px 3px; background:#3d4f7f; border-radius:6px; border:solid 1px #7087b3;}
+    .widget_map{ overflow:hidden;}
+    .widget_unique{ background:none; border:none !important;}  /*无边框无背景的*/
+    .widget_title{ width:100%; height:29px; border-bottom:solid 1px #7087b3; }  /*标题*/
+    .widget_title p{ line-height:27px; text-indent:20px; font-size:14px; letter-spacing:0.5px;}
+    /*模块******/
+    .padd_ydqk{ position:relative;}
+    /*湖北省用电情况********************************************************************************/
+    .padd_ydqk{ overflow:hidden;}
+    .ydqk_top{ display:block; overflow:hidden; padding:12px 15px;  background:#556698; border-bottom:solid 1px #677ca8;}
+    .ydqk_top ul{ float:left; }
+
+    /*三组数据*/
+    .ydqk_data{ overflow:hidden; float:right !important;}
+    .ydqk_data li{ float:left; }
+    .ydqk_data li p{ float:left; line-height:24px; font-size:12px;}
+    .ydqk_data li a{ float:left; width:126px; height:24px; background:#3d4f7f; border:solid 1px #283b67; text-indent:10px; border-radius: 1px; font-family:Arial, Helvetica, sans-serif; line-height:22px;}
+    .ydqk_data li span{ float:left; line-height:24px; margin-left:4px; font-size:12px;}
+    .ydqk_data li i{ float:left; width:1px; height:24px; background:#7087b3; margin:0 15px;}
+    /*三组数据*/
+    /*地图*/
+    .chars_ydqk{ float:left; width:80%; height:579px; background:url(../../assets/images/map_bg.jpg) no-repeat top;}
+    /*地图*/
+    /*地图右边表格*/
+    /*表格*/
+    table{ background:#6176a4; width:100%; border-collapse:collapse; border-spacing:0;  }
+    table td{ height:30px; line-height:30px; background:#3d4f7f; border:1px solid #6176a4; font-size:12px; color:#cbddfd; padding-left:10px;}
+    /*表格*/
+    .table_ydqk{ float:left; width:20%;  height:526px; margin-top:3px;}
+    .table_ydqk table td{ height:31px !important; line-height:20px !important;}
+    .table_ydqk table .table_title td{ background: #51618f !important; height:44px !important;  line-height:18px !important;}
+    /*地图右边表格*/
+    /*湖北省用电情况********************************************************************************/
+    .switch_btn_map{ position:absolute; top:15px; left:15px; z-index:10;}
+    .switch_btn{ overflow:hidden; float:left; width:190px; height:22px; border:solid 1px #8192c6; border-radius:2px; background:#3d4f7f;}
+    .switch_btn li{ float:left; width:25%; height:100%; line-height:20px; text-align:center; font-size:12px; border-left:solid 1px #8192c6; color:#91a2d7; cursor:pointer;}
+    .switch_btn li:hover{ color:#cbddfd !important; text-shadow: 0px 1px 0px #34405b;} /*切换按钮经过*/
+    .switch_all{ border-left:none !important;}
+    .switch_btn .switch_btn_click{ color:#cbddfd !important; text-shadow: 0px 1px 0px #34405b;} /*切换按钮点击*/
+
+</style>
